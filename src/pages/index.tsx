0e904138737d7cf5179970400cbedcff1a2f620c
Diego C.Sano <diego_ceccarelli@hotmail.com>
2022-01-04 22:01:43 -0300

    Fix file indent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import { GetStaticProps } from 'next';
-import Link from 'next/link';
-import { FiUser, FiCalendar } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-import Prismic from '@prismicio/client';
-import { getPrismicClient } from '../services/prismic';
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import Header from '../components/Header';
-
+import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -66,6 +65,7 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
+
   const postsResponse = await prismic.query(
     [Prismic.Predicates.at('document.type', 'posts')],
     {
@@ -74,6 +74,7 @@ export const getStaticProps: GetStaticProps = async () => {
   );
 
   const { next_page } = postsResponse;
+
   const posts = postsResponse.results.map(post => {
     return {
       uid: post.uid,
